d876bf96cd80f78b696e8037ea2eb42135fbc1d5
Ella Sophia <moraisellasophia@gmail.com>
2026-05-11 08:56:56 -0300

    novo

--- apps/clientes/views.py
+++ apps/clientes/views.py
@@ -1,32 +1,39 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
-from .forms import ClienteForm 
+# importação do login
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
+
+
+from .forms import ClienteForm
 from .models import Cliente
 
 # Create your views here.
 
+@login_required
 def novo_cliente(request):
-    clientes = Cliente.objects.all() 
+    clientes = Cliente.objects.all() # SELECT * FROM clientes
     template_name = 'novo_cliente.html'
     context = {}
     if request.method == 'POST':
         form = ClienteForm(request.POST)
         if form.is_valid():
-          form.save()
-          return redirect('novo_cliente')
+            form.save()
+            return redirect('novo_cliente')
         else:
             return HttpResponse('<h1>Deu erro no teu formulário<h1>')
-        
+    
     form = ClienteForm()
     context['form'] = form
-    context['clientes'] = clientes
-    
-    return render(request, template_name, context) 
+    context['clientes'] = clientes   
+    return render(request, template_name, context)
 
+@login_required
 def atualizar_cliente(request, id):
     try:
-        cliente = Cliente.objects.get(id=id)
+        cliente = Cliente.objects.get(id=id) # SELECT * FROM cliente WHERE ID = id
     except Cliente.DoesNotExist:
         return HttpResponse('<h1>Cliente não encontrado</h1>')
     
@@ -37,18 +44,43 @@ def atualizar_cliente(request, id):
             return redirect('novo_cliente')
         else:
             return HttpResponse('<h1>Erro na atualização do cliente</h1>')
-        
+    
     form = ClienteForm(instance=cliente)
     template_name = 'novo_cliente.html'
+    clientes = Cliente.objects.all()
     context = {
-        'form': form
+        'form': form,
+        'clientes': clientes
     }
     return render(request, template_name, context)
 
+@login_required
 def excluir_cliente(request, id):
     try:
         cliente = Cliente.objects.get(id=id)
         cliente.delete()
     except Cliente.DoesNotExist:
-        return HttpResponse('<h1>Erro ao exluir o cliente. Não Encontrado</h1>')
+        return HttpResponse('<h1>Erro ao excluir o cliente. Não encontrado</h1>')
     return redirect('novo_cliente')
+
+
+def login_usuario(request):
+    template_name = 'login.html'
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            usuario = authenticate(username=username, password=password)
+            
+            if usuario is not None:
+                login(request, usuario)
+                return redirect('novo_cliente')
+        else:
+            return HttpResponse(request, "Usuário ou senha inválidos.")
+    else:
+        form = AuthenticationForm()
+    
+    context = {'form': form}
+        
+    return render(request, template_name, context)
--- cliente/urls.py
+++ cliente/urls.py
@@ -2,7 +2,7 @@
 URL configuration for cliente project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -20,8 +20,9 @@ from django.urls import path
 from clientes import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls), 
+    path('admin/', admin.site.urls),
     path('atualizar-cliente/<int:id>/', views.atualizar_cliente, name='atualizar_cliente'),
     path('excluir-cliente/<int:id>/', views.excluir_cliente, name='excluir_cliente'),
+    path('login/', views.login_usuario, name='login_usuario'),
     path('', views.novo_cliente, name='novo_cliente'),
 ]
