9438758050ba75c9f26229f177a3a04b746a67b1
Ella Sophia <moraisellasophia@gmail.com>
2026-05-14 08:47:20 -0300

    DA CERTO PLMD DEUS

--- cliente/settings.py
+++ cliente/settings.py
@@ -129,3 +129,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATIC_URL = 'static/'
